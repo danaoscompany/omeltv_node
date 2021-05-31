d4b0d5b96fcfb37b25d0fdbaef5b36647d00fb2f
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2021-06-01 03:43:46 +0700

    Last commit

--- turn.js
+++ turn.js
@@ -1,2 +1,2 @@
 const {execSync} = require('child_process');
-execSync("export USERS='omeltv:c(8~_(<G&3caR>9,';export REALM=104.171.114.162;export UDP_PORT=3479;./simple-turn-linux-amd64");
+execSync("export USERS='omeltv:c(8~_(<G&3caR>9,';export REALM=baitmah.com;export UDP_PORT=3480;./simple-turn-linux-amd64");
